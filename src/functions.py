207d9e35f0de8695b5b9320ffc05540ae90f6f11
olallika <olaia.artieda@gmail.com>
2016-11-02 17:51:12 +0100

    Created rage comics. Removed the black border around rage comics.

--- src/functions.py
+++ src/functions.py
@@ -1,7 +1,11 @@
+import PIL
 import cv2
 import os
 import random
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv
 
 
 def loadMEMEs(path):
@@ -34,36 +38,66 @@ def createMEMEs(path, original_memes, meme_classes):
 
         print "Created new MEMEs."
 
-def createRageComic(path, original_memes, meme_classes, nRageComics):
-    # RESIZE IMAGES TO 250 x 250 or 250 x 750
+
+def resizeNEGATIVES(negative_images, new_size, save_folder):
+    neg_files = os.listdir(negative_images)
+
+    if not os.path.exists(save_folder + '/'):
+        os.mkdir(save_folder + '/')
+
+    cont = 0
+    for neg_file in neg_files:
+        print 'Resized: ' + str(cont) + '/' + str(len(neg_files))
+        if neg_file.endswith(".jpg"):
+            try:
+                img = Image.open(negative_images + '/' + neg_file)
+                img = img.resize(new_size, PIL.Image.ANTIALIAS)
+                img.save(save_folder + '/' + neg_file)
+
+            except IOError:
+                print neg_file
+
+        cont += 1
 
 
+def createRageComic(path, large_memes, small_memes, nRageComics):
     for i in range(0, nRageComics, 1):
         nRows = 3
         row = []
+        border_color = [0, 0, 0]
+        border_size = 1
 
-        for ii in range (0, nRows, 1):
-            image1_width = 0
-            image2_width = 0
+        for ii in range(0, nRows, 1):
+            row_type = random.random()
 
-            while image1_width != 250 and image2_width != 250:
-                row_img1 = random.choice(original_memes)
-                image1_width = row_img1.shape[1]
-                row_img2 = random.choice(original_memes)
-                image2_width = row_img2.shape[1]
+            if row_type >= 0.2:
+                # two small rage memes
+                meme_1 = random.choice(small_memes)
+                meme_1 = cv2.copyMakeBorder(meme_1, border_size, border_size, border_size, border_size,
+                                            cv2.BORDER_CONSTANT, value=border_color)
 
-            # ADD TEXT
-            # Font types: original_meme, rage_meme
-            font_type = "rage_meme"
-            row_img1 = addText(row_img1, font_type)
-            row_img2 = addText(row_img2, font_type)
+                meme_2 = random.choice(small_memes)
+                meme_2 = cv2.copyMakeBorder(meme_2, border_size, border_size, border_size, border_size,
+                                            cv2.BORDER_CONSTANT, value=border_color)
 
-            row.append(np.hstack([row_img1, row_img2]))
+                row.append(np.hstack([meme_1, meme_2]))
+
+            elif row_type < 0.2:
+                # one large rage meme
+                meme = random.choice(large_memes)
+                meme = cv2.copyMakeBorder(meme, border_size, border_size, border_size*2, border_size*2,
+                                          cv2.BORDER_CONSTANT, value=border_color)
+
+                row.append(np.hstack([meme]))
 
         comic = np.vstack([row[0], row[1], row[2]])
+        comic = comic[3:752, 5:499]
 
-    print "Created new MEMEs."
+        if not os.path.exists(path + 'rage_comics/'):
+            os.mkdir(path + 'rage_comics/')
+        cv2.imwrite(path + 'rage_comics/' + str(i) + '.jpg', comic)
 
+        print "Created " + str(i) + '/' + str(nRageComics)
 
 def addText(original_meme, font_type):
     if font_type == "original_meme":
@@ -86,8 +120,8 @@ def addText(original_meme, font_type):
 
     # Center the text
     # y = (h + textSize[0][1])/2
-    y = int(h - h*0.85)
-    x = int((w - textSize[0][0])/2)
+    y = int(h - h * 0.85)
+    x = int((w - textSize[0][0]) / 2)
 
     # Put text on the top
     # Put text on the bottom
--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
-from functions import loadMEMEs, createMEMEs, createRageComic
+from functions import loadMEMEs, createMEMEs, createRageComic, resizeNEGATIVES
 
-# CREATE ORIGINAL MEMES
-memes_path = "../../datasets/memes/"
-n_classes, original_memes, meme_classes = loadMEMEs(memes_path)
-memes_path = "../../datasets/syn_memes/"
-createMEMEs(memes_path, original_memes, meme_classes)
+# # CREATE ORIGINAL MEMES
+# memes_path = "../../datasets/memes/"
+# n_classes, original_memes, meme_classes = loadMEMEs(memes_path)
+# memes_path = "../../datasets/syn_memes/"
+# createMEMEs(memes_path, original_memes, meme_classes)
 
-# CREATE RAGE COMICS
-memes_path = "../../datasets/rage_memes/"
-_, original_memes, meme_classes = loadMEMEs(memes_path)
-memes_path = "../../datasets/syn_memes/"
+# # CREATE RAGE COMICS
+memes_path = "../../../datasets/rage_memes/"
 
-nRageComics = 50000
-createRageComic(memes_path, original_memes, meme_classes, nRageComics)
+# # RESIZE ALL IMAGES TO 250x250 AND 250x500
+# original_image_path = memes_path + 'large_memes/'
+# new_image_path = memes_path + 'large_memes_resized/'
+# new_size = (500, 250)
+# resizeNEGATIVES(original_image_path, new_size, new_image_path)
+# original_image_path = memes_path + 'small_memes/'
+# new_image_path = memes_path + 'small_memes_resized/'
+# new_size = (250, 250)
+# resizeNEGATIVES(original_image_path, new_size, new_image_path)
 
-print "Detected " + str(n_classes) + " meme classes.\n"
+_, large_memes, large_meme_classes = loadMEMEs(memes_path + 'large_memes_resized/')
+_, small_memes, small_meme_classes = loadMEMEs(memes_path + 'small_memes_resized/')
+
+memes_path = "../../../datasets/syn_memes/"
+nRageComics = 20
+createRageComic(memes_path, large_memes, small_memes, nRageComics)
+
+print "Detected " + str(large_meme_classes + small_meme_classes) + " meme classes.\n"
